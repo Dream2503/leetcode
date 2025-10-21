d548dd86cc2609f3b78288c2842d16e0be221a4a
Dream <swapnarajmohanty2503@gmail.com>
2025-10-21 16:34:08 +0530

    commit

--- 0-update.sh
+++ 0-update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Hardcoded merge headers script
 
 set -e
 
 DIR="/mnt/d/achha/Dream/leetcode"
 OUT="$DIR/all_in_one.hpp"
 
-# Create/empty the output file and add #pragma once
-echo "#pragma once" >> "$OUT"
 
+rm -f "$OUT"
+echo "#pragma once" >> "$OUT"
+echo >> "$OUT"
 
 find "$DIR" -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) ! -path "$OUT" | sort | while read -r file; do
         rel="${file#$DIR/}"
