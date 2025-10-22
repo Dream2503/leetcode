1108862ba1391407bd7ba95ed7b23b516811b71c
Dream <swapnarajmohanty2503@gmail.com>
2025-10-22 23:26:50 +0530

    commit

--- 0-update.sh
+++ 0-update.sh
@@ -9,7 +9,7 @@ rm -f "$OUT"
 echo "#pragma once" >> "$OUT"
 echo >> "$OUT"
 
-find "$DIR" -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) ! -path "$OUT" | sort | while read -r file; do
+find "$DIR" -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) ! -path "$OUT" | sort -r | while read -r file; do
     rel="${file#$DIR/}"
     rel="${rel//\\//}"
     echo "#include \"$rel\"" >> "$OUT"
