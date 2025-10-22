adff49a2747fbc3fd555374600062058ddd8aae0
Dream <swapnarajmohanty2503@gmail.com>
2025-10-22 21:46:55 +0530

    commit

---
+++ 938.range-sum-of-bst.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=938 lang=cpp
+ *
+ * [938] Range Sum of BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(const TreeNode* root, int low, int high) {
+        if (root) {
+            if (root->val >= low && root->val <= high) {
+                return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+            }
+            if (root->val < low) {
+                return rangeSumBST(root->right, low, high);
+            }
+            if (root->val > high) {
+                return rangeSumBST(root->left, low, high);
+            }
+        }
+        return 0;
+    }
+};
+// @lc code=end
