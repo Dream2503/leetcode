4c1d71ca7537324e61e8b011dd5e507c34fc13fa
Dream <swapnarajmohanty2503@gmail.com>
2025-10-26 19:40:10 +0530

    commit

---
+++ 1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1379 lang=cpp
+ *
+ * [1379] Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if (original && cloned) {
+            if (original == target) {
+                return cloned;
+            }
+            if (TreeNode* res = getTargetCopy(original->left, cloned->left, target)) {
+                return res;
+            }
+            if (TreeNode* res = getTargetCopy(original->right, cloned->right, target)) {
+                return res;
+            }
+        }
+        return nullptr;
+    }
+};
+// @lc code=end
