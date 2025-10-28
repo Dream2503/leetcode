798e83dd0fb1467cb4cd9a8b022b3cb2c2928b00
Dream <swapnarajmohanty2503@gmail.com>
2025-10-28 23:39:57 +0530

    commit

---
+++ 1523.count-odd-numbers-in-an-interval-range.cpp
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1523 lang=cpp
+ *
+ * [1523] Count Odd Numbers in an Interval Range
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countOdds(const int low, const int high) { return (high + 1) / 2 - low / 2; }
+};
+// @lc code=end
