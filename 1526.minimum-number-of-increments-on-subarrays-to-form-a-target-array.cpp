9d594d493959165ff9f92aee5382d5b2070a0b34
Dream <91144384+Dream2503@users.noreply.github.com>
2025-10-30 14:58:13 +0530

    commit

--- 1526.minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1526.minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -12,7 +12,7 @@ public:
         int res = target[0];
 
         for (int i = 1; i < size; i++) {
-            if (target[i] > target[i - 1]) {
+            if (target[i - 1] < target[i]) {
                 res += target[i] - target[i - 1];
             }
         }
