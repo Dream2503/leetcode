7a2d967d5c1001b7f60d030a2347dfa54b30b034
Dream <swapnarajmohanty2503@gmail.com>
2025-11-01 08:53:18 +0530

    commit

---
+++ 207.course-schedule.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=207 lang=cpp
+ *
+ * [207] Course Schedule
+ */
+
+// @lc code=start
+class Solution {
+    enum State { UNVISITED, VISITING, VISITED };
+
+    bool dfs(std::vector<std::vector<int>>& graph, std::vector<State>& state, const int source) {
+        state[source] = VISITING;
+
+        for (const int adj : graph[source]) {
+            if (state[adj] == VISITING) {
+                return false;
+            }
+            if (state[adj] == UNVISITED && !dfs(graph, state, adj)) {
+                return false;
+            }
+        }
+        state[source] = VISITED;
+        return true;
+    }
+
+public:
+    bool canFinish(const int numCourses, const std::vector<std::vector<int>>& prerequisites) {
+        std::vector<std::vector<int>> graph(numCourses);
+        std::vector state(numCourses, UNVISITED);
+
+        for (const std::vector<int>& edge : prerequisites) {
+            graph[edge[1]].push_back(edge[0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == UNVISITED && !dfs(graph, state, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
