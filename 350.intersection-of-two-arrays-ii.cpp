0653d709eb7da38dec5805bc413c9599f8f52d9c
Dream <swapnarajmohanty2503@gmail.com>
2025-11-01 11:11:20 +0530

    commit

--- 350.intersection-of-two-arrays-ii.cpp
+++ 350.intersection-of-two-arrays-ii.cpp
@@ -8,27 +8,12 @@
 class Solution {
 public:
     std::vector<int> intersect(std::vector<int>& nums1, std::vector<int>& nums2) {
-        std::sort(nums1.begin(), nums1.end());
-        std::sort(nums2.begin(), nums2.end());
+        std::ranges::sort(nums1);
+        std::ranges::sort(nums2);
         std::vector<int> res;
-        std::set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), std::back_inserter(res));
+        res.reserve(nums1.size() + nums2.size());
+        std::ranges::set_intersection(nums1, nums2, std::back_inserter(res));
         return res;
-
-        // const int size1 = nums1.size(), size2 = nums2.size();
-        // int i = 0, j = 0;
-
-        // while (i < size1 && j < size2) {
-        //     if (nums1[i] < nums2[j]) {
-        //         i++;
-        //     } else if (nums1[i] > nums2[j]) {
-        //         j++;
-        //     } else {
-        //         res.push_back(nums1[i]);
-        //         i++;
-        //         j++;
-        //     }
-        // }
-        // return res;
     }
 };
 // @lc code=end
