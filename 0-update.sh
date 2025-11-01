38b773bda458519f630b7b8845079ab40cd39314
Dream <swapnarajmohanty2503@gmail.com>
2025-11-01 19:01:13 +0530

    commit

--- 0-update.sh
+++ 0-update.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-
 set -e
 
 DIR="/mnt/d/achha/Dream/leetcode"
 OUT="$DIR/all_in_one.hpp"
 
-rm -f "$OUT"
-echo "#pragma once" >> "$OUT"
-echo >> "$OUT"
+{
+    echo "#pragma once"
+    echo
 
-find "$DIR" -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) ! -path "$OUT" | sort | while read -r file; do
-    rel="${file#$DIR/}"
-    rel="${rel//\\//}"
-    echo "#include \"$rel\"" >> "$OUT"
-done
+    for f in "$DIR"/*.cpp; do
+        [ -f "$f" ] && echo "#include \"${f#$DIR/}\""
+    done
+} > "$OUT"
