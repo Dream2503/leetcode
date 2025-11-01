6b3d6c20d49d204ce906bdfe11894fe325ce6cf3
Dream <swapnarajmohanty2503@gmail.com>
2025-11-01 11:07:29 +0530

    commit

--- 349.intersection-of-two-arrays.cpp
+++ 349.intersection-of-two-arrays.cpp
@@ -8,28 +8,14 @@
 class Solution {
 public:
     std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
-        std::sort(nums1.begin(), nums1.end());
-        nums1.resize(std::unique(nums1.begin(), nums1.end()) - nums1.begin());
-        std::sort(nums2.begin(), nums2.end());
-        nums2.resize(std::unique(nums2.begin(), nums2.end()) - nums2.begin());
+        std::ranges::sort(nums1);
+        nums1.erase(std::ranges::unique(nums1).begin(), nums1.end());
+        std::ranges::sort(nums2);
+        nums2.erase(std::ranges::unique(nums2).begin(), nums2.end());
         std::vector<int> res;
-        std::set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), std::back_inserter(res));
+        res.reserve(nums1.size() + nums2.size());
+        std::ranges::set_intersection(nums1, nums2, std::back_inserter(res));
         return res;
-
-        // const int size1 = nums1.size(), size2 = nums2.size();
-        // int i = 0, j = 0;
-        //
-        // while (i < size1 && j < size2) {
-        //     if (nums1[i] < nums2[j]) {
-        //         i++;
-        //     } else if (nums1[i] > nums2[j]) {
-        //         j++;
-        //     } else {
-        //         res.push_back(nums1[i]);
-        //         j++;
-        //     }
-        // }
-        // return res;
     }
 };
 // @lc code=end
