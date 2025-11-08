b0afca3de1154ce009bede7d0f7d62c72dc84f53
Dream <swapnarajmohanty2503@gmail.com>
2025-11-08 12:02:05 +0530

    commit

--- 200.number-of-islands.cpp
+++ 200.number-of-islands.cpp
@@ -7,6 +7,35 @@
 // @lc code=start
 class Solution {
 public:
-    int numIslands(const std::vector<std::vector<char>>& grid) {}
+    int numIslands(std::vector<std::vector<char>>& grid) {
+        const int row_size = grid.size(), col_size = grid.front().size();
+        int res = 0;
+
+        for (int i = 0; i < row_size; i++) {
+            for (int j = 0; j < col_size; j++) {
+                if (grid[i][j] == '1') {
+                    std::queue<std::pair<int, int>> queue;
+                    grid[i][j] = '0';
+                    queue.emplace(i, j);
+
+                    while (!queue.empty()) {
+                        const auto [x, y] = queue.front();
+                        queue.pop();
+
+                        for (const auto& [dx, dy] : {std::pair(-1, 0), {0, -1}, {0, 1}, {1, 0}}) {
+                            const int nx = x + dx, ny = y + dy;
+
+                            if (nx >= 0 && nx < row_size && ny >= 0 && ny < col_size && grid[nx][ny] == '1') {
+                                grid[nx][ny] = '0';
+                                queue.emplace(nx, ny);
+                            }
+                        }
+                    }
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
 };
 // @lc code=end
