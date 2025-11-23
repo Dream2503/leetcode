37a5a40f05308bdd31b61119769aa0f99a5e4125
Dream <swapnarajmohanty2503@gmail.com>
2025-11-23 11:16:35 +0530

    commit

--- 33.search-in-rotated-sorted-array.cpp
+++ 33.search-in-rotated-sorted-array.cpp
@@ -7,31 +7,30 @@
 // @lc code=start
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int idx = 0, size = nums.size();
-        if (size == 1) return nums[0] == target ? 0 : -1;
-        for (; nums[idx] < nums[idx + 1] and idx < size - 1; idx++);
-        int low = 0, act_low = ++idx, high = size - 1, act_high = idx - 1, mid, act_mid, temp;
+    int search(const std::vector<int>& nums, const int target) {
+        const int size = nums.size();
+        int low = 0, high = size - 1;
 
-        do {
-            temp = low + high;
-            mid = temp / 2;
-            act_mid = temp % 2 ? (mid) - idx : (mid) - idx + 1;
-            if (act_mid >= size) act_mid -= size;
-            if (act_mid < 0) act_mid += size;
-            if (act_low >= size) act_low -= size;
-            if (act_high < 0) act_high += size;
-            if (nums[act_mid] == target) return act_mid;
-            else if (nums[act_mid] > target) {
-                act_high = act_mid - 1;
-                high = mid - 1;
-            } else {
-                act_low = act_mid + 1;
+        while (low < high) {
+            const int mid = (high + low) / 2;
+
+            if (nums[mid] > nums[high]) {
                 low = mid + 1;
+            } else {
+                high = mid;
             }
         }
+        const auto itr1 = std::lower_bound(nums.begin(), nums.begin() + low, target);
 
+        if (itr1 != nums.begin() + low && *itr1 == target) {
+            return itr1 - nums.begin();
+        }
+        const auto itr2 = std::lower_bound(nums.begin() + low, nums.end(), target);
+
+        if (itr2 != nums.end() && *itr2 == target) {
+            return itr2 - nums.begin();
+        }
+        return -1;
     }
 };
 // @lc code=end
-
